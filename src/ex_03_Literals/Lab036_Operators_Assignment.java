76c75cd94654ccde0984ac2bbaa7f6903baaa420
kazinurjahan <kazinurjhan@gmail.com>
2025-09-28 23:04:18 +0530

    14th Sept Part 2

---
+++ src/ex_03_Literals/Lab036_Operators_Assignment.java
@@ -0,0 +1,21 @@
+package ex_03_Literals;
+
+public class Lab036_Operators_Assignment {
+    public static void main(String[] args) {
+
+
+        int age = 90;
+        // Assignment operator
+        // int -> data type
+        // age -> identifier
+        // = -> Assignment operator
+        // 90 -> Literal
+
+        // RHV to the LH
+
+
+
+
+
+    }
+}
