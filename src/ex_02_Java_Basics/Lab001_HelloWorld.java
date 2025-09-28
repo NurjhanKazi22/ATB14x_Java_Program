1672539c14c275ffc6bfa032ec12c7ffc2c241e2
kazinurjahan <kazinurjhan@gmail.com>
2025-09-28 23:35:15 +0530

    13th Sept 2025, all files added part 1

---
+++ src/ex_02_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+
+
+    }
+}
