d3f2f5c3d45a7f42da3b5a8ff84623e21ba13fdb
kazinurjahan <kazinurjhan@gmail.com>
2025-09-28 22:12:05 +0530

    Push 28th Sept Tasks

---
+++ src/ex_10_For_Loop/Lab117_While_Loop.java
@@ -0,0 +1,13 @@
+package ex_10_For_Loop;
+
+public class Lab117_While_Loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i > 0) {
+            //
+            System.out.println("Enter");
+            System.out.println(i);
+            i++;
+        }
+    }
+}
