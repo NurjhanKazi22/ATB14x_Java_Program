b7c98bca2c6439cd60052aa07a08d12d544ab0de
kazinurjahan <kazinurjhan@gmail.com>
2025-10-04 23:15:14 +0530

    4th oct 2025

---
+++ src/ex_13_Functions/Lab135_String_Immutable.java
@@ -0,0 +1,15 @@
+package ex_13_Functions;
+
+public class Lab135_String_Immutable {
+    public static void main(String[] args) {
+
+        String name =  "Pramod";
+        name.toUpperCase(); // PRAMOD
+        System.out.println(name); // PRAMOD
+
+
+
+
+
+    }
+}
---
+++ src/ex_13_Functions/Lab139_Strings_Interview.java
@@ -0,0 +1,9 @@
+package ex_13_Functions;
+
+public class Lab139_Strings_Interview {
+    public static void main(String[] args) {
+        String name = "pramod";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
