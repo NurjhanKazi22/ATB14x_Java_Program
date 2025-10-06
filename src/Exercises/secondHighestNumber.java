f24776b6bd7fcc5a5d6b29a03c8a6b53c510424a
kazinurjahan <kazinurjhan@gmail.com>
2025-10-06 23:31:31 +0530

    edge cases

--- src/Exercises/secondHighestNumber.java
+++ src/Exercises/secondHighestNumber.java
@@ -2,19 +2,27 @@ package Exercises;
 
 public class secondHighestNumber {
     public static void main(String[] args) {
-      int[] integerArray = {12, 34, 10, 1, 100, 3, 4, 32};
+      int[] integerArray = {12, 32, 10, 1, 100, 3, 4, 34};
       int firstHighest = Integer.MIN_VALUE;
       int secondHigh = Integer.MIN_VALUE;
 
       for(int i = 0;i<integerArray.length;i++){
-          int temp =0;
-          if(firstHighest<=integerArray[i]){
-              temp = firstHighest;
+
+          if(firstHighest<integerArray[i]){
+              secondHigh  = firstHighest;
               firstHighest=integerArray[i];
-              secondHigh = temp;
+          }else if(secondHigh<integerArray[i] && integerArray[i]!=firstHighest){
+              secondHigh=integerArray[i];
           }
+
+//          if(firstHighest<=integerArray[i]){
+//              secondHigh = firstHighest;
+//              firstHighest=integerArray[i];
+//          }
       }
-        //System.out.println("first highest number"+firstHighest);
+
+
+        System.out.println("first highest number: "+firstHighest);
         System.out.println("Second highest number: "+secondHigh);
     }
 }
