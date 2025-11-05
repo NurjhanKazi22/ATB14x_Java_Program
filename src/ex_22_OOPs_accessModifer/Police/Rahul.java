e97d8e2feca5244f1aed77ec18e7f63d32cf7951
kazinurjahan <kazinurjhan@gmail.com>
2025-11-05 23:05:28 +0530

    26 th oct task

---
+++ src/ex_22_OOPs_accessModifer/Police/Rahul.java
@@ -0,0 +1,12 @@
+package ex_22_OOPs_accessModifer.Police;
+
+public class Rahul extends Cop{
+    public Rahul(int bullet) {
+        super(bullet);
+    }
+
+    public static void main(String[] args) {
+        Cop p = new Cop(100);
+        p.thisDefaultF1();
+    }
+}
---
+++ src/superKeyword/Lab181_Super.java
@@ -0,0 +1,35 @@
+package superKeyword;
+
+public class Lab181_Super {
+
+}
+
+class Father{
+    Father() {
+        System.out.println("DC Father");
+    }
+
+    int gold = 10;
+
+    void home() {
+        System.out.println("Home Father");
+    }
+
+}
+
+class Son extends Father {
+    Son(){
+        super();
+    }
+    int gold_c = 100;
+    void bike(){}
+
+    void newHome() {
+        System.out.println(super.gold);
+        super.home();
+        this.bike();
+        System.out.println(this.gold_c);
+    }
+
+
+}
